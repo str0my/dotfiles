76e182032be8e56acc8b1b071d087bace5445806
stromy <26250916+str0my@users.noreply.github.com>
2017-11-25 15:53:02 +0100

    Fix install scripts

---
+++ install/tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Setting up tmux..."
+
+curl -s https://raw.githubusercontent.com/str0my/dotfiles/master/.tmux.conf > ~/.tmux.conf
--- install/zsh.sh
+++ install/zsh.sh
@@ -3,7 +3,8 @@
 echo "Setting up zsh..."
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-curl -s https://raw.githubusercontent.com/saelo/dotfiles/master/zshrc > ~/.zshrc
+curl -s https://raw.githubusercontent.com/str0my/dotfiles/master/.zshrc > ~/.zshrc
+curl -s https://raw.githubusercontent.com/str0my/dotfiles/master/themes/stromy.zsh-theme > ~/.oh-my-zsh/themes/stromy.zsh-theme
 
 # needs sudo to work passwordless
 sudo chsh -s $(which zsh) $(whoami)
